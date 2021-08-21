a50768fe03ebe69ad483ddc9cd5387a54e9a3d87
Lenosey <Lenosey@foxmail.com>
2021-08-21 17:24:29 +0800

    8.21

--- jjalloc.cpp
+++ jjalloc.cpp
@@ -1,362 +1,318 @@
-#include <new>
-#include <cstdlib>
 #include <climits>
-#include <iostream>
 #include <cstddef>
-#include <vector>
+#include <cstdlib>
 #include <cstring>
+#include <iostream>
+#include <new>
+#include <vector>
+
+using std::cerr;
 using std::cout;
 using std::endl;
-using std::cerr;
 
 namespace shuxin {
 
 #if 1
-    template<class T>
-    inline T* _allocate(ptrdiff_t size, T*)
-    {
-        std::set_new_handler(0); //当new失败时，呼叫该函数所设的参数
-        T* tmp = (T*)(::operator new((size_t)(size * sizeof(T))));
-        if (tmp == 0)
-        {
-            cerr << "out of memory" << endl;
-            exit(1);
-        }
-        return tmp;
-    }
+template <class T> inline T *_allocate(ptrdiff_t size, T *) {
+  std::set_new_handler(0); //当new失败时，呼叫该函数所设的参数
+  T *tmp = (T *)(::operator new((size_t)(size * sizeof(T))));
+  if (tmp == 0) {
+    cerr << "out of memory" << endl;
+    exit(1);
+  }
+  return tmp;
+}
 
-    template<class T>
-    inline void _deallocate(T* buffer)
-    {
-        ::operator delete(buffer);
-    }
+template <class T> inline void _deallocate(T *buffer) {
+  ::operator delete(buffer);
+}
 
-    template<class T1, class T2>
-    inline void _construct(T1* p, const T2& value)
-    {
-        new (p) T1(value);
-    }
+template <class T1, class T2> inline void _construct(T1 *p, const T2 &value) {
+  new (p) T1(value);
+}
 
-    template<class T>
-    inline void _destroy(T* ptr)
-    {
-        ptr->~T();
-    }
+template <class T> inline void _destroy(T *ptr) { ptr->~T(); }
 
-    template<class T>
-    class allocator
-    {
-    public:
-        typedef T value_type;
-        typedef T* pointer;
-        typedef const T* const_pointer;
-        typedef T& reference;
-        typedef const T& const_reference;
-        typedef size_t size_type;
-        typedef ptrdiff_t difference_type;
+template <class T> class allocator {
+public:
+  typedef T value_type;
+  typedef T *pointer;
+  typedef const T *const_pointer;
+  typedef T &reference;
+  typedef const T &const_reference;
+  typedef size_t size_type;
+  typedef ptrdiff_t difference_type;
 
-        template<class U>
-        struct rebind
-        {
-            typedef allocator<U> other;
-        };
+  template <class U> struct rebind { typedef allocator<U> other; };
 
-        pointer allocate(size_type n, const void* hint = 0)
-        {
-            return _allocate((difference_type)n, (pointer)0);
-        }
+  pointer allocate(size_type n, const void *hint = 0) {
+    return _allocate((difference_type)n, (pointer)0);
+  }
 
-        void deallocate(pointer p, size_type n)
-        {
-            _deallocate(p);
-        }
+  void deallocate(pointer p, size_type n) { _deallocate(p); }
 
-        void construct(pointer p, const T& value)
-        {
-            _construct(p, value);
-        }
+  void construct(pointer p, const T &value) { _construct(p, value); }
 
-        void destroy(pointer p)
-        {
-            _destroy(p);
-        }
+  void destroy(pointer p) { _destroy(p); }
 
+  void destroy(pointer start, pointer end) {
+    for (pointer it = start; it != end; ++it)
+      _destroy(it);
+  }
 
-        pointer address(reference x)
-        {
-            return (pointer)&x;
-        }
+  pointer address(reference x) { return (pointer)&x; }
 
-        const_pointer const_address(const_reference x)
-        {
-            return (const_pointer)&x;
-        }
+  const_pointer const_address(const_reference x) { return (const_pointer)&x; }
 
-        size_type max_size() const
-        {
-            return size_type(UINT_MAX / sizeof(T));
-        }
+  size_type max_size() const { return size_type(UINT_MAX / sizeof(T)); }
 
-        pointer _Zero_range(pointer first, pointer last) {
-            for (; first != last; ++first)
-                (*first) = 0;
-            return last;
-        }
-    };
-    
-    template<>
-    void allocator<char>::destroy(char* p) {
-        cout << "char*" << endl;
-        _destroy(p);
-    }
+  pointer _Zero_range(pointer first, pointer last) {
+    for (; first != last; ++first)
+      (*first) = 0;
+    return last;
+  }
+};
 
-    template<>
-    char* allocator<char>::_Zero_range(char* first,char* last) {
-        cout << "char*" << endl;
-        memset(first, 0, static_cast<size_t>(last - first));
-        return last;
-    }
+template <> void allocator<char>::destroy(char *p) {
+  cout << "char*" << endl;
+  _destroy(p);
+}
+
+template <> char *allocator<char>::_Zero_range(char *first, char *last) {
+  cout << "char*" << endl;
+  memset(first, 0, static_cast<size_t>(last - first));
+  return last;
+}
 #endif
-#if 0
-	class malloc_alloc {
-	public:
-		static void* allocate(size_t);
-		static void* reallocate(void*, size_t);
-		static void deallocate(void*, size_t);
-		static void (*set_new_handler(void (*)()))();
-	private:
-		static void (*oom_handler)();
-		static void* oom_alloc(size_t);
-		static void* oom_realloc(void*, size_t);
-	};
+#if 1
+class malloc_alloc {
+public:
+  static void *allocate(size_t);
+  static void *reallocate(void *, size_t);
+  static void deallocate(void *, size_t);
+  static void (*set_new_handler(void (*)()))();
 
-	void (*malloc_alloc::oom_handler)() = 0;
+private:
+  static void (*oom_handler)();
+  static void *oom_alloc(size_t);
+  static void *oom_realloc(void *, size_t);
+};
 
-	void* malloc_alloc::allocate(size_t n) {
-		cout << "maloc_alloc" << endl;
-		void* ret = malloc(n);
-		if (!ret)
-			ret = oom_alloc(n);
-		return ret;
-	}
+void (*malloc_alloc::oom_handler)() = 0;
 
-	void* malloc_alloc::reallocate(void* p, size_t n) {
-		void* ret = realloc(p, n);
-		if (!ret)
-			ret = oom_realloc(p, n);
-		return ret;
-	}
+void *malloc_alloc::allocate(size_t n) {
+  cout << "maloc_alloc" << endl;
+  void *ret = malloc(n);
+  if (!ret)
+    ret = oom_alloc(n);
+  return ret;
+}
 
-	void malloc_alloc::deallocate(void* p, size_t n) {
-		free(p);
-	}
+void *malloc_alloc::reallocate(void *p, size_t n) {
+  void *ret = realloc(p, n);
+  if (!ret)
+    ret = oom_realloc(p, n);
+  return ret;
+}
 
-	void (*malloc_alloc::set_new_handler(void (*f)()))() {
-		void (*old)() = oom_handler;
-		oom_handler = f;
-		return old;
-	}
+void malloc_alloc::deallocate(void *p, size_t n) { free(p); }
 
-	void* malloc_alloc::oom_alloc(size_t n) {
-		void* ret;
-		for (;;) {
-			if (!oom_handler)
-				exit(1);
-			(*oom_handler)();
-			ret = malloc(n);
-			if (ret)
-				return ret;
-		}
-	}
+void (*malloc_alloc::set_new_handler(void (*f)()))() {
+  void (*old)() = oom_handler;
+  oom_handler = f;
+  return old;
+}
 
-	void* malloc_alloc::oom_realloc(void* p, size_t n) {
-		void* ret;
-		for (;;) {
-			if (!oom_handler)
-				exit(1);
-			(*oom_handler)();
-			ret = realloc(p, n);
-			if (ret)
-				return ret;
-		}
-	}
+void *malloc_alloc::oom_alloc(size_t n) {
+  void *ret;
+  for (;;) {
+    if (!oom_handler)
+      exit(1);
+    (*oom_handler)();
+    ret = malloc(n);
+    if (ret)
+      return ret;
+  }
+}
 
-	static const size_t __ALIGN = 8;
-	static const size_t __MAX_BYTES = 128;
-	static const size_t __FREELISTS_NUMS = __MAX_BYTES / __ALIGN;
+void *malloc_alloc::oom_realloc(void *p, size_t n) {
+  void *ret;
+  for (;;) {
+    if (!oom_handler)
+      exit(1);
+    (*oom_handler)();
+    ret = realloc(p, n);
+    if (ret)
+      return ret;
+  }
+}
 
-	union obj {
-		union obj* next_free_list;	//下一个freelist
-		char client_data[1];	//用户能看到的
-	};
+static const size_t __ALIGN = 8;
+static const size_t __MAX_BYTES = 128;
+static const size_t __FREELISTS_NUMS = __MAX_BYTES / __ALIGN;
 
-	class pool_alloc {
-	public:
-		static void* allocate(size_t);
-		static void deallocate(void*, size_t);
+union obj {
+  union obj *next_free_list; //下一个freelist
+  char client_data[1];       //用户能看到的
+};
 
-	private:
-		static size_t ROUND_UP(size_t);
-		static size_t FREELIST_INDEX(size_t);
+class pool_alloc {
+public:
+  static void *allocate(size_t);
+  static void deallocate(void *, size_t);
 
-	public:
-		static obj* free_list[__FREELISTS_NUMS];
-		static char* start_pos;
-		static char* end_pos;
-		static size_t pool_size;
+private:
+  static size_t ROUND_UP(size_t);
+  static size_t FREELIST_INDEX(size_t);
 
-	private:
-		static void* refill(size_t);
-		static char* chunk_alloc(size_t, int&);
-	};
+public:
+  static obj *free_list[__FREELISTS_NUMS];
+  static char *start_pos;
+  static char *end_pos;
+  static size_t pool_size;
 
-	size_t pool_alloc::pool_size = 0;
-	char* pool_alloc::start_pos = 0;
-	char* pool_alloc::end_pos = 0;
+private:
+  static void *refill(size_t);
+  static char *chunk_alloc(size_t, int &);
+};
 
-	obj* pool_alloc::free_list[__FREELISTS_NUMS] = {
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0
-	};
+size_t pool_alloc::pool_size = 0;
+char *pool_alloc::start_pos = 0;
+char *pool_alloc::end_pos = 0;
 
-	size_t pool_alloc::ROUND_UP(size_t n) {
-		return (((n)+__ALIGN - 1) & ~(__ALIGN - 1));
-	}
+obj *pool_alloc::free_list[__FREELISTS_NUMS] = {0, 0, 0, 0, 0, 0, 0, 0,
+                                                0, 0, 0, 0, 0, 0, 0, 0};
 
-	size_t pool_alloc::FREELIST_INDEX(size_t n) {
-		return (((n)+__ALIGN - 1) / __ALIGN - 1);
-	}
+size_t pool_alloc::ROUND_UP(size_t n) {
+  return (((n) + __ALIGN - 1) & ~(__ALIGN - 1));
+}
 
-	void* pool_alloc::allocate(size_t n) {
-		cout << " memory pool"<<endl;
-		if (n > __MAX_BYTES)
-			return malloc_alloc::allocate(n);
+size_t pool_alloc::FREELIST_INDEX(size_t n) {
+  return (((n) + __ALIGN - 1) / __ALIGN - 1);
+}
 
-		obj** ptr_free_list;
-		obj* result;
-		ptr_free_list = free_list + FREELIST_INDEX(n);
-		result = *ptr_free_list;
+void *pool_alloc::allocate(size_t n) {
+  cout << " memory pool" << endl;
+  if (n > __MAX_BYTES)
+    return malloc_alloc::allocate(n);
 
-		if (!result) {
-			void* r = refill(ROUND_UP(n));
-			return r;
-		}
-		*ptr_free_list = result->next_free_list;
+  obj **ptr_free_list;
+  obj *result;
+  ptr_free_list = free_list + FREELIST_INDEX(n);
+  result = *ptr_free_list;
 
-		return (result);
-	}
+  if (!result) {
+    void *r = refill(ROUND_UP(n));
+    return r;
+  }
+  *ptr_free_list = result->next_free_list;
 
-	void pool_alloc::deallocate(void* p, size_t n) {
-		if (n > __MAX_BYTES || !n) {
-			malloc_alloc::deallocate(p, n);
-			return;
-		}
+  return (result);
+}
 
-		obj* q = (obj*)p;
-		obj** ptr_free_list = free_list + FREELIST_INDEX(n);
-		q->next_free_list = *ptr_free_list;
-		*ptr_free_list = q;
-	}
+void pool_alloc::deallocate(void *p, size_t n) {
+  if (n > __MAX_BYTES || !n) {
+    malloc_alloc::deallocate(p, n);
+    return;
+  }
 
-	void* pool_alloc::refill(size_t n) {
-		int nobj = 15;
-		char* chunk = chunk_alloc(n, nobj);
+  obj *q = (obj *)p;
+  obj **ptr_free_list = free_list + FREELIST_INDEX(n);
+  q->next_free_list = *ptr_free_list;
+  *ptr_free_list = q;
+}
 
-		if (nobj == 1)
-			return (chunk);
+void *pool_alloc::refill(size_t n) {
+  int nobj = 15;
+  char *chunk = chunk_alloc(n, nobj);
 
-		obj* curr;
-		obj** ptr_free_list = free_list + FREELIST_INDEX(n);
+  if (nobj == 1)
+    return (chunk);
 
-		*ptr_free_list = curr = (obj*)(chunk + n);
-		int len = nobj - 1;
-		for (int i = 1; i < len; i++) {
-			curr->next_free_list = (obj*)((char*)curr + n);
-			curr = curr->next_free_list;
-		}
-		curr->next_free_list = 0;
-		return (chunk);
-	}
+  obj *curr;
+  obj **ptr_free_list = free_list + FREELIST_INDEX(n);
 
-	char* pool_alloc::chunk_alloc(size_t n, int& nobj) {
-		size_t pool_left = end_pos - start_pos;
-		char* ret;
-		if (pool_left >= n * nobj) {
-			ret = start_pos;
-			start_pos += n * nobj;
-			return ret;
-		}
-		else if (pool_left >= n) {
-			nobj = pool_left / n;
-			ret = start_pos;
-			start_pos += n * nobj;
-			return ret;
-		}
-		else {
-			size_t bytes_needed = (n << 1) * nobj + ROUND_UP(pool_size >> 4);
-			if (pool_left > 0) {
-				obj** ptr_free_list = free_list + FREELIST_INDEX(pool_left);
-				((obj*)start_pos)->next_free_list = *ptr_free_list;
-				*ptr_free_list = (obj*)start_pos;
-			}
-			start_pos = (char*)malloc(bytes_needed);
-			if (!start_pos) {
-				obj** ptr_free_list;
-				obj* p;
-				for (int i = n; i < __MAX_BYTES; i += __ALIGN) {
-					ptr_free_list = free_list + FREELIST_INDEX(i);
-					p = *ptr_free_list;
-					if (p) {
-						*ptr_free_list = p->next_free_list;
-						start_pos = (char*)p;
-						end_pos = start_pos + i;
-						return (chunk_alloc(n, nobj));
-					}
-				}
-				end_pos = 0;
-				start_pos = (char*)malloc_alloc::allocate(bytes_needed);
-			}
-			pool_size += bytes_needed;
-			end_pos = start_pos + bytes_needed;
-			return chunk_alloc(n, nobj);
-		}
-	}
+  *ptr_free_list = curr = (obj *)(chunk + n);
+  int len = nobj - 1;
+  for (int i = 1; i < len; i++) {
+    curr->next_free_list = (obj *)((char *)curr + n);
+    curr = curr->next_free_list;
+  }
+  curr->next_free_list = 0;
+  return (chunk);
+}
 
-	typedef pool_alloc alloc;
+char *pool_alloc::chunk_alloc(size_t n, int &nobj) {
+  size_t pool_left = end_pos - start_pos;
+  char *ret;
+  if (pool_left >= n * nobj) {
+    ret = start_pos;
+    start_pos += n * nobj;
+    return ret;
+  } else if (pool_left >= n) {
+    nobj = pool_left / n;
+    ret = start_pos;
+    start_pos += n * nobj;
+    return ret;
+  } else {
+    size_t bytes_needed = (n << 1) * nobj + ROUND_UP(pool_size >> 4);
+    if (pool_left > 0) {
+      obj **ptr_free_list = free_list + FREELIST_INDEX(pool_left);
+      ((obj *)start_pos)->next_free_list = *ptr_free_list;
+      *ptr_free_list = (obj *)start_pos;
+    }
+    start_pos = (char *)malloc(bytes_needed);
+    if (!start_pos) {
+      obj **ptr_free_list;
+      obj *p;
+      for (int i = n; i < __MAX_BYTES; i += __ALIGN) {
+        ptr_free_list = free_list + FREELIST_INDEX(i);
+        p = *ptr_free_list;
+        if (p) {
+          *ptr_free_list = p->next_free_list;
+          start_pos = (char *)p;
+          end_pos = start_pos + i;
+          return (chunk_alloc(n, nobj));
+        }
+      }
+      end_pos = 0;
+      start_pos = (char *)malloc_alloc::allocate(bytes_needed);
+    }
+    pool_size += bytes_needed;
+    end_pos = start_pos + bytes_needed;
+    return chunk_alloc(n, nobj);
+  }
+}
+
+typedef pool_alloc alloc;
 #endif
-	template<typename T, typename Alloc = alloc>
-	class simple_alloc {
-	public:
-		typedef T value_type;
-		typedef T* pointer;
-		typedef const T* const_pointer;
-		typedef T& reference;
-		typedef const T& const_reference;
-		typedef size_t size_type;
-		typedef ptrdiff_t difference_type;
+template <typename T, typename Alloc = alloc> class simple_alloc {
+public:
+  typedef T value_type;
+  typedef T *pointer;
+  typedef const T *const_pointer;
+  typedef T &reference;
+  typedef const T &const_reference;
+  typedef size_t size_type;
+  typedef ptrdiff_t difference_type;
 
-		template<class U>
-		struct rebind
-		{
-			typedef simple_alloc<U> other;
-		};
+  template <class U> struct rebind { typedef simple_alloc<U> other; };
 
-		static T* allocate(const size_t n) {
-			return n ? (T*)Alloc::allocate(n * sizeof(T)) : 0;
-		}
-		static void deallocate(T* p, const size_t n) {
-			Alloc::deallocate(p, n * sizeof(T));
-		}
-	};
+  static T *allocate(const size_t n) {
+    return n ? (T *)Alloc::allocate(n * sizeof(T)) : 0;
+  }
+  static void deallocate(T *p, const size_t n) {
+    Alloc::deallocate(p, n * sizeof(T));
+  }
+};
 } // namespace shuxin
 
-
-int main()
-{
-    std::vector<int, shuxin::allocator<int>>my_vector;
-    for (int i = 0; i < 10; i++)
-        my_vector.push_back(i);
-    for (auto it : my_vector)
-        cout << it << endl;
-    return 0;
-}
+// int main()
+// {
+//     std::vector<int, shuxin::simple_alloc<int>>my_vector;
+//     for (int i = 0; i < 10; i++)
+//         my_vector.push_back(i);
+//     for (auto it : my_vector)
+//         cout << it << endl;
+//     return 0;
+// }
---
+++ my_vector.cpp
@@ -0,0 +1,134 @@
+#include "jjalloc.cpp"
+#include <iostream>
+#include <memory>
+
+using std::cout;
+using std::endl;
+
+template <typename T, typename Alloc = shuxin::pool_alloc> class my_vector {
+
+public:
+  typedef T value_type;
+  typedef value_type *pointer;
+  typedef value_type *iterator;
+  typedef value_type &reference;
+  typedef const value_type &const_reference;
+  typedef ptrdiff_t difference_type;
+  typedef size_t size_type;
+
+protected:
+  typedef shuxin::simple_alloc<value_type, Alloc> data_allocator;
+  iterator start;
+  iterator finish;
+  iterator end_of_storage; //可用空间的尾
+
+  void deallocate() {
+    if (start)
+      data_allocator::deallocate(start, end_of_storage - start);
+  }
+  void fill_initialize(size_type n, const T &value) {
+    start = allocate_and_fill(n, value);
+    finish = start + n;
+    end_of_storage = finish;
+  }
+
+public:
+  iterator begin() { return start; }
+  iterator end() { return finish; }
+  size_type size()  { return size_type(end() - begin()); }
+  size_type capacity()  { return size_type(end_of_storage - begin()); }
+  bool empty() const { return begin() == end(); }
+  reference operator[](size_type n) { return *(begin() + n); }
+  my_vector() : start(0), finish(0), end_of_storage(0) {}
+  my_vector(size_type n, const T &value) { fill_initialize(n, value); }
+  my_vector(int n, const T &value) { fill_initialize(n, value); }
+  my_vector(long n, const T &value) { fill_initialize(n, value); }
+  explicit my_vector(size_type n) { fill_initialize(n, T()); }
+
+  ~my_vector() {
+    std::destroy(start, finish);
+    deallocate();
+  }
+  reference front() { return *begin(); }
+  reference back() { return *(end() - 1); }
+  void push_back(const T &x) {
+    if (finish != end_of_storage) {
+      shuxin::_construct(finish, x);
+      ++finish;
+    } else
+      insert_aux(end(), x);
+  }
+  void push_back(const T &&x) {
+    if (finish != end_of_storage) {
+      shuxin::_construct(finish, x);
+      ++finish;
+    } else
+      insert_aux(end(), x);
+  }
+  void pop_back() {
+    --finish;
+    std::destroy(finish);
+  }
+  iterator erase(iterator position) {
+    if (position + 1 != end())
+      std::copy(position + 1, finish, position);
+    --finish;
+    std::destroy_at(finish);
+    return position;
+  }
+  void resize(size_type new_size, const T &x) {
+    if (new_size < size())
+      erase(begin() + new_size, end());
+    else
+      insert(end(), new_size - size(), x);
+  }
+  void resize(size_type new_size) { resize(new_size, T()); }
+  void clear() { erase(begin(), end()); }
+
+protected:
+  iterator allocate_and_fill(size_type n, const T &x) {
+    iterator result = data_allocator::allocate(n);
+    uninitialized_fill_n(result, n, x);
+    return result;
+  }
+  void insert_aux(iterator position, const T &x) {
+    if (finish != end_of_storage) // 还有备用空间
+    {
+      shuxin::_construct(finish, *(finish - 1));
+      ++finish;
+      T x_copy = x;
+      std::copy_backward(position, finish - 2, finish - 1);
+      // copy [_First, _Last) backwards to [..., _Dest)
+      *position = x_copy;
+    } else // 已无备用空间
+    {
+      const size_type old_size = size();
+      const size_type len = old_size != 0 ? 2 * old_size : 1;
+
+      iterator new_start = data_allocator::allocate(len);
+      iterator new_finish = new_start;
+      new_finish = std::uninitialized_copy(start, position, new_start);
+      shuxin::_construct(new_finish, x);
+      ++new_finish;
+      new_finish = std::uninitialized_copy(position, finish, new_finish);
+      std::destroy(begin(), end());
+      deallocate();
+
+      start = new_start;
+      finish = new_finish;
+      end_of_storage = new_start + len;
+    }
+  }
+};
+
+int main() {
+  my_vector<int> a;
+  a.push_back(3);
+  a.push_back(4);
+  a.push_back(5);
+  a.erase(a.begin() + 1);
+  for (auto it : a)
+    cout << it << " ";
+  cout << endl;
+  return 0;
+}

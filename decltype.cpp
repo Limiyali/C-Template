bd493504edcc55ff07d83e1ad1463046c5c4f058
Lenosey <Lenosey@foxmail.com>
2021-09-23 22:22:59 +0800

    9.23

--- decltype.cpp
+++ decltype.cpp
@@ -39,43 +39,43 @@ int main() {
   cout << "\n\n\nfirst---------------\n";
   const int a = 0;
   f(a);
-  cout << "\n\n\n引用---------------\n";
+  // cout << "\n\n\n引用---------------\n";
   int x = 27;
   int &rx = x;
-  f2(x);
-  f2(rx);
+  // f2(x);
+  // f2(rx);
 
-  cout << "\n\n\n指针---------------\n";
-  int *px = &x;
-  f3(&x);
-  f3(px);
+  // cout << "\n\n\n指针---------------\n";
+  // const int *px = &x;
+  // f3(&x);
+  // f3(px);
 
-  cout << "\n\n\n右值引用---------------\n";
+  // cout << "\n\n\n右值引用---------------\n";
   const int cx = x;
-  f4(x);
-  f4(cx);
-  f4(rx);
-  f4(27);
+  // f4(x);
+  // f4(cx);
+  // f4(rx);
+  // f4(27);
 
-  cout << "\n\n\n值传递---------------\n";
-  x = 27;
-  volatile const &cvx = x;
-  f5(x);
-  f5(cx);
-  f5(rx);
-  f5(cvx);
+  // cout << "\n\n\n值传递---------------\n";
+  // x = 27;
+  // volatile const &cvx = x;
+  // f5(x);
+  // f5(cx);
+  // f5(rx);
+  // f5(cvx);
 
-  cout << "\n\n\n退化---------------\n";
-  const char name[] = "shuxin.wang";
-  const char *ptrToName = name;
-  cout << abi::__cxa_demangle(typeid(name).name(), nullptr, nullptr, nullptr)
-       << endl;
-  cout << abi::__cxa_demangle(typeid(ptrToName).name(), nullptr, nullptr,
-                              nullptr)
-       << endl;
-  cout << arraySize(name) << endl;
+  // cout << "\n\n\n退化---------------\n";
+  // const char name[] = "shuxin.wang";
+  // const char *ptrToName = name;
+  // cout << abi::__cxa_demangle(typeid(name).name(), nullptr, nullptr, nullptr)
+  //      << endl;
+  // cout << abi::__cxa_demangle(typeid(ptrToName).name(), nullptr, nullptr,
+  //                             nullptr)
+  //      << endl;
+  // cout << arraySize(name) << endl;
 
-  cout << "\n\n\n函数指针---------------\n";
-  f2(someFunc);
-  f5(someFunc);
+  // cout << "\n\n\n函数指针---------------\n";
+  // f2(someFunc);
+  // f5(someFunc);
 }
